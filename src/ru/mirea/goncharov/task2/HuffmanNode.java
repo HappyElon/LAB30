38bf67a118957eccf284ec1c677f9b1c65e386bb
Artem <artemgoncharov6003@gmail.com>
2023-12-17 12:07:10 +0300

    17.12.23

---
+++ src/ru/mirea/goncharov/task2/HuffmanNode.java
@@ -0,0 +1,19 @@
+package ru.mirea.goncharov.task2;
+
+class HuffmanNode implements Comparable<HuffmanNode> {
+    char data;
+    int frequency;
+    HuffmanNode left, right;
+
+    public HuffmanNode(char data, int frequency) {
+        this.data = data;
+        this.frequency = frequency;
+        this.left = null;
+        this.right = null;
+    }
+
+    @Override
+    public int compareTo(HuffmanNode node) {
+        return this.frequency - node.frequency;
+    }
+}
